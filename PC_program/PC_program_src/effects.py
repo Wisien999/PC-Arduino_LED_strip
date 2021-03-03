b9c844d17c68253603c6d66273450bc0476548de
Wisien <wisnia3221@gmail.com>
2021-03-03 19:25:42 +0100

    Create traveling pixels effect not pulsating color

--- PC_program/PC_program_src/effects.py
+++ PC_program/PC_program_src/effects.py
@@ -1,3 +1,4 @@
+from typing import Any
 from PC_program_src import summary_tools
 from PC_program_src.constances import EffectsIDs
 from PC_program_src import config
@@ -8,8 +9,8 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 def set_last_led(effect_config, value):
     effect_config['last_led'] = value
 
-def set_property(ef_property, value, effect_config):
-    effect_config[ef_property] = value
+def set_property(efffect_property: str, value: Any, effect_config: dict):
+    effect_config[efffect_property] = value
 
 def load_default_color(hue: QtWidgets.QDoubleSpinBox, sat: QtWidgets.QDoubleSpinBox, val: QtWidgets.QDoubleSpinBox, color: dict):
     hue.setValue(color['hue'])
@@ -61,11 +62,9 @@ class Fill_solid(Effect):
         # Update logical configuration (self.effect_config) on visible ones change
         last_led_widget.valueChanged.connect(lambda: set_last_led(self.effect_config, int(last_led_widget.value())))
 
-        def set_color_prop(prop, prop_src):
-            self.effect_config['color'][prop] = int(prop_src.value())
-        hue.valueChanged.connect(lambda: set_color_prop('hue', hue))
-        sat.valueChanged.connect(lambda: set_color_prop('saturation', sat))
-        val.valueChanged.connect(lambda: set_color_prop('value', val))
+        hue.valueChanged.connect(lambda: set_property('hue', int(hue.value()), self.effect_config['color']))
+        sat.valueChanged.connect(lambda: set_property('saturation', int(sat.value()), self.effect_config['color']))
+        val.valueChanged.connect(lambda: set_property('value', int(val.value()), self.effect_config['color']))
 
 
     def prepare_command(self):
@@ -115,24 +114,46 @@ class Linear_colors(Effect):
 
         return prefix + ",".join(map(str, args))
 
-class Pulsating_color(Effect):
+class Traveling_pixels(Effect):
     def __init__(self, place: int, summary_model: QtGui.QStandardItemModel, summary_widget: QtWidgets.QTreeView, effects_config: dict, effect_type: EffectsIDs) -> None:
         super().__init__(place, summary_model, summary_widget, effects_config, effect_type)
 
     
     def make_summary(self):
         # Create summary items and configuration widgets
+        root = self.summary_model.invisibleRootItem()
         last_led_widget = summary_tools.CREATE_LAST_LED_ITEM(self.summary_model, self.summary_widget)
         transition_time_widget = summary_tools.CREATE_TRANSITION_TIME_ITEM(self.summary_model, self.summary_widget)
+        pixel_spacing_widget = summary_tools.CREATE_ITEM_WITH_NUMBER("Pixel spacing", config.NUM_OF_LEDS, root, self.summary_model, self.summary_widget)
+        pixel_spacing_widget.setDecimals(0)
+        blur_radius_widget = summary_tools.CREATE_ITEM_WITH_NUMBER("Blur radius", config.NUM_OF_LEDS, root, self.summary_model, self.summary_widget)
+        blur_radius_widget.setDecimals(0)
         
         color1_hue, color1_sat, color1_val = summary_tools.CREATE_ITEM_WITH_COLOR("Traveling pixel color:", self.summary_model.invisibleRootItem(), self.summary_model, self.summary_widget)
         color2_hue, color2_sat, color2_val = summary_tools.CREATE_ITEM_WITH_COLOR("Background color", self.summary_model.invisibleRootItem(), self.summary_model, self.summary_widget)
         
         # Visualize default effect configuration
-        last_led_widget.setValue(self.effect_config.get('last_led'))
+        last_led_widget.setValue(self.effect_config['last_led'])
         transition_time_widget.setValue(self.effect_config['transition_time'])
+        pixel_spacing_widget.setValue(self.effect_config['pixel_spacing'])
+        blur_radius_widget.setValue(self.effect_config['blur_radius'])
+
         load_default_color(color1_hue, color1_sat, color1_val, self.effect_config['color'])
         load_default_color(color2_hue, color2_sat, color2_val, self.effect_config['background_color'])
+        
+        # Update logical configuration (self.effect_config) on visible ones change
+        last_led_widget.valueChanged.connect(lambda: set_last_led(self.effect_config, int(last_led_widget.value())))
+        transition_time_widget.valueChanged.connect(lambda: set_property('transition_time', int(transition_time_widget.value()), self.effect_config))
+        pixel_spacing_widget.valueChanged.connect(lambda: set_property('pixel_spacing', int(pixel_spacing_widget.value()), self.effect_config))
+        blur_radius_widget.valueChanged.connect(lambda: set_property('blur_radius', int(blur_radius_widget.value()), self.effect_config))
+
+        color1_hue.valueChanged.connect(lambda: set_property('hue', int(color1_hue.value()), self.effect_config['color']))
+        color1_sat.valueChanged.connect(lambda: set_property('saturation', int(color1_sat.value()), self.effect_config['color']))
+        color1_val.valueChanged.connect(lambda: set_property('value', int(color1_val.value()), self.effect_config['color']))
+
+        color2_hue.valueChanged.connect(lambda: set_property('hue', int(color2_hue.value()), self.effect_config['background_color']))
+        color2_sat.valueChanged.connect(lambda: set_property('saturation', int(color2_sat.value()), self.effect_config['background_color']))
+        color2_val.valueChanged.connect(lambda: set_property('value', int(color2_val.value()), self.effect_config['background_color']))
 
     
     def prepare_command(self):
@@ -152,5 +173,5 @@ class Pulsating_color(Effect):
 create_effect = {
     EffectsIDs.FILL_SOLID: Fill_solid,
     EffectsIDs.LINEAR_COLORS: Linear_colors,
-    EffectsIDs.PULSATING_COLOR: Pulsating_color
+    EffectsIDs.TRAVELING_PIXELS: Traveling_pixels
 }
